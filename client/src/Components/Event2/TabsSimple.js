d1039cd0d1284115c5522cd8350bd49578a86511
anjuman raj <ayusmann23@gmail.com>
2022-04-22 14:46:33 +0530

    final

--- client/src/Components/Event2/TabsSimple.js
+++ client/src/Components/Event2/TabsSimple.js
@@ -20,7 +20,7 @@ export default function TabsSimple({ branch, setBranch }) {
       <div className="text-sm font-medium text-center text-gray-500  dark:border-gray-700">
         <ul className="flex flex-wrap justify-center -mb-px">
           {
-            ["CORE" , "CSE" , "ISE" ,"CORE" , "CSE" , "ISE" , "CORE" , "CSE" , "ISE" ,  ].map((ele , i) =>
+            ["AIML","BT","CV", "CSE","CCE","EEE","ECE","ISE","ME" ].map((ele , i) =>
             
             
             <li className="mr-2" key={i}>
