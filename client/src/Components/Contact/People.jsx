3cefecae503efecbcb13da0721df281ed650e567
Thanmay <mdasthanmay@gmail.com>
2022-04-22 21:52:31 +0530

    init Team section

---
+++ client/src/Components/Contact/People.jsx
@@ -0,0 +1,12 @@
+const People = () => {
+  return (
+    <>
+      <div className="">
+        <img src="" alt="" />
+      </div>
+      <div className=""></div>
+    </>
+  );
+};
+2;
+export default People;
