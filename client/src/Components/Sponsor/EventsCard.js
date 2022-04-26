ab1080dacdf59829cec9f9bd6bc33ed6eddb54cc
dhanishssuvarna <dhanishssuvarna123@gmail.com>
2022-04-26 19:18:25 +0530

    sponsor UI update

--- client/src/Components/Sponsor/EventsCard.js
+++ client/src/Components/Sponsor/EventsCard.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './sponsor.css';
 
-
-function EventsCard({ src, SponsorTitle, SponsorDesc, SponsorWebsiteLink }) {
-
-
-    return (
-        <>
-
-            <div className="flex flex-col p-0 sm:flex-col border-0 overflow-hidden  items-center bg-[#171717 ]  md:flex-row md:max-w-xl   min-w-[90%] h-[50vh]  ">
-
-
-                <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-1/2 md:rounded-none md:rounded-l-lg" src={src} alt="" />
-
-                <div className="flex flex-col justify-left p-10 overflow-visible leading-normal">
-                    <h5 className="mb-2 text-5xl font-bold tracking-tight text-[#EDEDED] text-left py-5">{SponsorTitle}</h5>
-                    <p className="mb-3 font-normal text-gray-300 ">{SponsorDesc}</p>
-                    <a href={SponsorWebsiteLink}>
-                    <button type="button" class="text-white bg-[#DA0037] hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-none text-sm px-5 py-2.5 text-center mr-2 my-2 max-w-fit   ">Go to Website</button>
-                    </a>
-                </div>
-            </div>
-
-
-        </>
-    )
-}
+const EventsCard = ({
+	ImageSrc,
+	SponsorTitle,
+	SponsorDesc,
+	SponsorWebsiteLink,
+}) => {
+	return (
+		<>
+			{console.log(ImageSrc, SponsorTitle, SponsorDesc, SponsorWebsiteLink)}
+			<div className='event__card'>
+				<div className='event__img__wrapper'>
+					<img className='event__img' src={ImageSrc} alt='' />
+				</div>
+				<div className='event__content_wrapper'>
+					<h5 className='event__heading text-3xl'>{SponsorTitle}</h5>
+					<p className='event__description'>{SponsorDesc}</p>
+					<a href={SponsorWebsiteLink}>
+						<button
+							type='button'
+							className='text-white bg-[#DA0037] hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-none text-sm px-5 py-2.5 text-center mr-2 my-2 max-w-fit   '
+						>
+							Go to Website
+						</button>
+					</a>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default EventsCard;
