6637e899a316429098fa873464bbe2b8dc2e3978
anjuman raj <ayusmann23@gmail.com>
2022-04-27 22:30:50 +0530

    ..

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,105 +1,4 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { GiHamburgerMenu } from "react-icons/gi";
 
-// import logo from "../Images/logo.png";
-
-// function Navbar({ tab }) {
-//   const [click, setClick] = useState(0);
-//   const handleHamburger = () => {
-//     if (click === 0) {
-//       document.getElementById("mobile-menu-4").classList.add("hidden");
-//       document.getElementById("mobile-menu-4").classList.add("visible");
-
-//       setClick(1);
-//     } else {
-//       document.getElementById("mobile-menu-4").classList.add("visible");
-//       document.getElementById("mobile-menu-4").classList.remove("hidden");
-
-//       setClick(0);
-//     }
-//   };
-//   return (
-//     <>
-//       <nav className="bg-[#171717] px-2 sm:px-4 py-4 h-1/6">
-//         <div className="container flex flex-wrap justify-between items-center mx-auto ">
-//           <Link to="/" className="flex items-center">
-//             <img src={logo} className="mx-3 h-6 sm:h-16" alt="Incridea Logo" />
-//             {/* <span className="self-center text-xl font-semibold whitespace-nowrap ">Incridea</span> */}
-//           </Link>
-//           <div className="flex md:order-2">
-//             <button
-//               type="button"
-//               class="rounded-md text-white bg-[#9d7643] focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 text-md font-bold px-5 py-2.5 text-center mr-2 mb-2"
-//             >
-//               {" "}
-//               <Link to="/login">LOGIN/REGISTER</Link>{" "}
-//             </button>
-//           </div>
-//         </div>
-
-//         <div>
-//           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-//             <li>
-//               <Link
-//                 to="/"
-//                 className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-//                   tab === "home" ? "text-[#9d7643]" : "text-[#EDEDED]"
-//                 } `}
-//                 aria-current="page"
-//               >
-//                 HOME
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/events"
-//                 className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-//                   tab === "events" ? "text-[#9d7643]" : "text-[#EDEDED]"
-//                 } `}
-//               >
-//                 EVENTS
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/gallery"
-//                 className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-//                   tab === "gallery" ? "text-[#9d7643]" : "text-[#EDEDED]"
-//                 } `}
-//               >
-//                 GALLERY
-//               </Link>
-//             </li>
-//             <li>
-//               <Link
-//                 to="/sponsors"
-//                 className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-//                   tab === "sponsor" ? "text-[#9d7643]" : "text-[#EDEDED]"
-//                 } `}
-//               >
-//                 SPONSORS
-//               </Link>
-//             </li>
-
-//             <li>
-//               <Link
-//                 to="/team"
-//                 className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-//                   tab === "about" ? "text-[#9d7643]" : "text-[#EDEDED]"
-//                 } `}
-//               >
-//                 ABOUT US
-//               </Link>
-//             </li>
-//           </ul>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// }
-
-// export default Navbar;
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -124,7 +23,7 @@ function Navbar({ tab }) {
     <>
       {/* {... click===0? onClick()=>setClick(1):onClick()=>setClick(0)}  */}
 
-      <nav className="bg-[#000] px-2 sm:px-4 py-6 font-nav min-w-fit ">
+      <nav className="bg-[#000] px-2 md:px-4 py-6 font-nav min-w-fit ">
         <div className="container text-center flex flex-wrap justify-between items-center  content-center mx-auto">
           <Link to="/" className="flex items-center">
             <img
@@ -135,12 +34,13 @@ function Navbar({ tab }) {
             {/* <span className="self-center text-xl font-semibold whitespace-nowrap ">Incridea</span> */}
           </Link>
           <div className="flex md:order-2">
-            <button
-              type="button"
-              class="rounded-none text-white bg-[#9d7643] font-sans focus:ring-4 focus:outline-none  text-sm md:text-lg font-bold px-3 py-2.5 text-center mr-1 mb-2 hover:text-white ">
-              
-              <Link to="/login">login / register</Link>
-            </button>
+            <Link  to="/login" >
+              <button
+                type="button"
+                class="rounded-none text-white bg-[#9d7643] font-sans focus:ring-4 focus:outline-none  text-sm md:text-lg font-bold px-1 md:px-3 py-2.5 text-center mr-1 mb-2 hover:text-white ">
+                login / register
+              </button>
+            </Link>
 
             <button
               data-collapse-toggle="mobile-menu-4"
@@ -185,9 +85,8 @@ function Navbar({ tab }) {
               <li>
                 <Link
                   to="/"
-                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-                    tab === "home" ? "text-[#9d7643]" : "text-[#EDEDED]"
-                  } `}
+                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${tab === "home" ? "text-[#9d7643]" : "text-[#EDEDED]"
+                    } `}
                   aria-current="page"
                 >
                   home
@@ -196,9 +95,8 @@ function Navbar({ tab }) {
               <li>
                 <Link
                   to="/events"
-                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-                    tab === "events" ? "text-[#9d7643]" : "text-[#EDEDED]"
-                  } `}
+                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${tab === "events" ? "text-[#9d7643]" : "text-[#EDEDED]"
+                    } `}
                 >
                   events
                 </Link>
@@ -206,9 +104,8 @@ function Navbar({ tab }) {
               <li>
                 <Link
                   to="/gallery"
-                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-                    tab === "gallery" ? "text-[#9d7643]" : "text-[#EDEDED]"
-                  } `}
+                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${tab === "gallery" ? "text-[#9d7643]" : "text-[#EDEDED]"
+                    } `}
                 >
                   gallery
                 </Link>
@@ -216,9 +113,8 @@ function Navbar({ tab }) {
               <li>
                 <Link
                   to="/sponsors"
-                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-                    tab === "sponsors" ? "text-[#9d7643]" : "text-[#EDEDED]"
-                  } `}
+                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${tab === "sponsors" ? "text-[#9d7643]" : "text-[#EDEDED]"
+                    } `}
                 >
                   sponsors
                 </Link>
@@ -227,9 +123,8 @@ function Navbar({ tab }) {
               <li>
                 <Link
                   to="/team"
-                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${
-                    tab === "team" ? "text-[#9d7643]" : "text-[#EDEDED]"
-                  } `}
+                  className={`block py-2 pr-4 pl-3  border-b border-gray-100 text-xl   hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#9d7643] md:p-0 ${tab === "team" ? "text-[#9d7643]" : "text-[#EDEDED]"
+                    } `}
                 >
                   about us
                 </Link>
--- client/src/Components/Sponsor/index.js
+++ client/src/Components/Sponsor/index.js
@@ -10,28 +10,32 @@ const SponsorDetails = [
   {
     name: "Coding Ninjas",
     description:
-      "Coding Ninjas is a place that trains passionate people in various technologies. Our core programs are intensive, immersive training that transforms people into outstanding developers.",
+      `At Coding Ninjas' mission is to continuously innovate the best ways to train the next generation of developers and to transform the the way tech education is delivered.
+      Coding Ninjas boasts of world-class teaching faculty and a state-of-art learning platform for Coding education with faculty alumni of IIT, Stanford, IIIT and Facebook. Coding Ninjas teaches 17+ Programming courses in Foundation,l Advanced, Data & Development courses such as Machine Learning, Data Science, Web Development, Android and more. Today, Coding Ninjas ecosystem comprises of 40,000+ students and alumni, 1000+ Campus Ambassadors, 2000+ Teaching Assistants, and 150+ employees.
+      `,
     image: codeNinja,
     url: "https://www.codingninjas.com/",
   },
   {
     name: "Red FM",
     description:
-      "Red FM is an Indian FM radio network headquartered in Chennai and owned by Chennai-based Sun Group. Red FM 93.5 is India's only Hindi CHR FM radio network spread across 48 cities.",
+      `RED FM* Network (RED FM + SURYAN FM) through its presence across 64 cities caters to over 40 million listeners. We have been recently rated as India's most admired FM radio station by the ASF. Over the years we have won more than 175 awards, across various forums, including the prestigious EMVIES, ABBY's, Goa Fest, New York Radio Festival, IRF, Golden Mikes etc. Red FM offers advertisers a number of distinctive opportunities to reach their target audiences using any of our specialized verticals. Our advertising team offers you opportunities to drive your communication through our multiple verticals.`,
     image: redfm,
     url: "https://www.redfmindia.in",
   },
   {
     name: "Lifestyle",
     description:
-      "Originating in India in 1999, Lifestyle is an Emirati retail fashion brand which comes under Dubai-based retail and hospitality conglomerate, the Landmark Group.",
+      `Lifestyle International (P) Ltd. opened its first Lifestyle store in India in 1999 and in a little over a decade has come to be recognized amongst leading retail companies in the country.
+      It is a part of the multi-billion dollar Dubai based retail and hospitality conglomerate Landmark Group. Positioned as a trendy, youthful and vibrant store, Lifestyle offers consumers an enjoyable shopping experience. Each Lifestyle store brings together multiple concepts under one roof - Apparel, Footwear, Children's Wear, Beauty & Accessories offering a convenient one stop shop and a choice of leading national & international brands to customers.`,
     image: lifestyle,
     url: "https://www.lifestylestores.com/in/en/",
   },
   {
     name: "Dlithe",
     description:
-      "Being agile is the need of hour. The world is on disruptive mode. The world is looking for agile workforce that are adoptable and produce quicker results.",
+      `Being agile is the need of hour. The world is on disruptive mode. The world is looking for agile workforce that are adoptable and produce quicker results. While academics and work provides required knowledge and experience, it is necessary for individuals to bridge gaps in skills. 
+      Our vision is to build an agile workforce which is competent in “Domain, Technology and Personality” towards readiness of global industry needs. Upgrade your skill and be competent to work in agile world. Explore your journey with us`,
     image: dlithe,
     url: "https://dlithe.com/",
   },
@@ -44,10 +48,10 @@ const Sponsor = () => {
       <Navbar tab="sponsor" />
       <div className="bg-black ">
         <div className="text-center pt-2">
-          <h1 className="text-5xl md:text-6xl tracking-widest  font-bold text-[#EDEDED]">
+          <h1 className="text-5xl md:text-6xl tracking-widest pb-5 font-bold text-[#EDEDED]">
             Our Sponsors
           </h1>
-          <div className="">
+          <div className=" gap-5">
             {SponsorDetails.map((sponorDetail) => (
               <EventsCard
                 ImageSrc={sponorDetail.image}
@@ -59,7 +63,7 @@ const Sponsor = () => {
           </div>
         </div>
       </div>
-      {console.log(SponsorDetails)}
+      {/* {console.log(SponsorDetails)} */}
     </>
   );
 };
