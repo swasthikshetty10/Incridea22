797de0e0d75556ee7f848ef79c93ba02efad9079
anjuman raj <ayusmann23@gmail.com>
2022-04-28 13:12:19 +0530

    ...

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -11,15 +11,17 @@ function Home() {
 
       <div className="flex pt-5 items-center justify-center flex-col-reverse md:flex-row ">
         <div className="flex gap-3 px-5  flex-col">
-          <img src={higher} className=" "  alt="" />
-          <p className="text-gray-100 text-lg pb-10" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nam laboriosam, nulla amet sint similique consequuntur iste enim fuga, magni consequatur repellat consectetur architecto ipsam quasi at culpa nemo perferendis illo odit omnis magnam? Maxime magni corporis natus mollitia distinctio tempora laborum fugit officia! Quis, architecto. Voluptas deleniti excepturi sunt.</p>
-
+          <img src={higher} className=" " alt="" />
+          <p className="text-gray-100 text-lg  text-justify" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nam laboriosam, nulla amet sint similique consequuntur iste enim fuga, magni consequatur repellat consectetur architecto ipsam quasi at culpa nemo perferendis illo odit omnis magnam? Maxime magni corporis natus mollitia distinctio tempora laborum fugit officia! Quis, architecto. Voluptas deleniti excepturi sunt.</p>
+          <div className="bg-white border-0 rounded-md px-6 py-2 w-fit font-bold  text-center " >
+          Registrations Starts 29th April
+          </div>
         </div>
 
-          <div className="min-w-fit">
-              {/* <img className="absolute top-20 left-5 z-10 md:top-8 md:left-[50rem] md:h-[90vh] " src={circle} alt="" /> */}
-            <img className="md:max-w-[50vw]" src={avatar} alt="" />
-          </div>
+        <div className="min-w-fit">
+          {/* <img className="absolute top-20 left-5 z-10 md:top-8 md:left-[50rem] md:h-[90vh] " src={circle} alt="" /> */}
+          <img className="md:max-w-[50vw]" src={avatar} alt="" />
+        </div>
 
 
       </div>
