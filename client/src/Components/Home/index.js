b36d385f7671e50dd940f909f1bbcadfa58ce4e1
Numan Naeem <numan.naeem9@gmail.com>
2022-04-30 00:38:13 +0530

    Fog and tranisitions added, chakras aligned

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -4,7 +4,7 @@ import higher from "../../Images/home/higher.png"
 import avatar from "../../Images/home/avatar.png"
 import Counter from "./Counter";
 import { Slide, Fade } from 'react-awesome-reveal'
-import cloud from './cloud1.png'
+// import cloud from './cloud1.png'
 import chakra from './chakra.png'
 function Home() {
   const [rotation, setRotation] = useState("rotate-0")
@@ -19,16 +19,28 @@ function Home() {
       <Fade duration={1500}>
         <Navbar tab="home" />
       </Fade>
-      <div className="overflow-x-hidden relative flex py-5 items-center justify-center flex-col-reverse md:flex-row tracking-wide" style={{ fontFamily: "CinzelDecorative-Bold" }}>
+      <div className="overflow-x-hidden relative  flex py-5 items-center justify-center flex-col-reverse md:flex-row tracking-wide" style={{ fontFamily: "CinzelDecorative-Bold" }}>
+        {/* Fog divs */}
+        <div id="foglayer_01" class="fog">
+          <div class="image01"></div>
+          <div class="image02"></div>
+        </div>
+        <div id="foglayer_02" class="fog">
+          <div class="image01"></div>
+          <div class="image02"></div>
+        </div>
+        <div id="foglayer_03" class="fog">
+          <div class="image01"></div>
+        <div class="image02"></div>
+        </div>
         <Slide direction="left" duration={1000}>
-          <div className="flex gap-3 px-5  flex-col" >
+          <div className="flex gap-3 px-5 flex-col" >
             <img src={higher} className=" " alt="" />
             <p className="text-gray-100 text-lg  text-justify" >An amalgamation of all forms of technical, literary and cultural activities Incridea is a celebration of fervour and talent. It is the national level techno-cultural fest of
-              <a target="blank" className="  text-[#cd9b58] hover:text-red-500 text-base" href="https://nmamit.nitte.edu.in/"> NMAM Institute of Technology</a> , Nitte .
+              <a target="blank" className="  text-[#cd9b58] hover:text-red-500 text-base" href="https://nmamit.nitte.edu.in/"> NMAM Institute of Technology</a>, Nitte.
             </p>
             <div className="select-none bg-white border-0 hover:bg-opacity-100  bg-opacity-10 text-white px-6 py-2 w-fit font-bold  text-center " >
-              Registrations start on 29th April
-
+              Registrations begin on 29th April
             </div>
           </div>
         </Slide>
@@ -38,24 +50,24 @@ function Home() {
             <img className="md:max-w-[50vw]" src={avatar} alt="" />
           </div>
         </Slide>
-        <Slide className="w-full h-full absolute -z-10" direction="left" duration={3500} delay={100}>
-          <img className=" md:h-full w-96 sm:w-full top-0 right-0  translate-x-12  h-64 sm: md:translate-x-72 lg:translate-x-96 md:scale-80" src={cloud} alt="" />
-        </Slide>
+        {/* <Slide className="w-full h-full absolute -z-10" direction="left" duration={3500} delay={100}>
+          <img className="custom-animation md:h-full w-96 sm:w-full top-0 right-0  translate-x-12  h-64 sm: md:translate-x-72 lg:translate-x-96 md:scale-80" src={cloud} alt="" />
+        </Slide> */}
 
       </div>
       <div className="relative overflow-hidden pt-36 pb-96">
         <div className="absolute w-full flex justify-between ">
-          <div className=" shrink-0  -ml-[395px]  -mt-28" >
-            <img style={{ transform: `rotate(-${rotation})` }} className={` h-[40rem]   w-auto`} src={chakra} alt="" />
+          <div className=" shrink-0 -ml-[305px] md:-ml-[395px]  " >
+            <img style={{ transform: `rotate(-${rotation})` }} className={`h-[30rem] md:h-[40rem] w-auto`} src={chakra} alt="" />
           </div>
-          <div className="shrink-0 -mr-[395px] hidden md:block  pt-10">
+          <div className="shrink-0 -mr-[395px] hidden md:block -mt-[15px]">
             <img style={{ transform: `rotate(${rotation})` }} className="h-[40rem] w-auto" src={chakra} alt="" />
           </div>
         </div>
         <Slide direction="up">
           <div className="flex justify-center text-center items-center py-5" style={{ fontFamily: "CinzelDecorative-Bold" }}>
-            <div className="flex flex-col items-center justify-center pt-20 ">
-              <h2 className="text-3xl sm:text-4xl lg:text-5xl p-3 font-semibold text-[#9d7643]">Countdown Has Begun</h2>
+            <div className="flex flex-col items-center justify-center pt-20 -mt-36 md:m-0 ">
+              <h2 className="text-3xl sm:text-4xl lg:text-6xl m-3 text-[#9d7643] p-2 sm:p-3 lg:p-4 bg-opacity-30 backdrop-blur-lg rounded-md bg-gray-900">Going live in</h2>
               <Counter setSecond={secSecond} />
             </div>
           </div>
--- client/src/Components/Topbutton.js
+++ client/src/Components/Topbutton.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { AiOutlineArrowUp } from 'react-icons/ai';
-import {Link} from "react-router-dom"
-// import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import React, { useEffect } from 'react'
+import { AiFillHome } from 'react-icons/ai';
+import { useNavigate } from "react-router-dom"
 
+function Topbutton({ topref, showBtn }) {
 
-function Topbutton({ topref }) {
-    const executeScroll = () => topref.current.scrollIntoView()
+    const navigate = useNavigate()
+    const executeScroll = () => {
+        navigate('/')
+    }
     return (
         <>
-            <button className="fixed bottom-2 right-5 inline-flex items-center justify-center  mb-2 mr-2 overflow-hidden font-medium text-gray-900 rounded-lg bg-black z-50">
-
-                <Link to="/gallery"
-                    // onclick={executeScroll}
+            <button style={{display: showBtn ? 'initial': 'none'}} onClick={executeScroll} className="fixed bottom-2 right-5 inline-flex items-center justify-center  mb-2 mr-2 overflow-hidden font-medium text-gray-900 rounded-lg bg-black z-50">
+                <p
                     className='relative px-2  p-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'
                 >
-                    <span className="">
-                        <AiOutlineArrowUp />
+                    <span>
+                        <AiFillHome />
                     </span>
-                </Link>
+                </p>
             </button>
         </>
     )
