019c30968f495acbe6b868db0563a946ddb64c12
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-04-30 20:58:59 +0530

    pic update

--- client/src/Components/TechTeam/data.js
+++ client/src/Components/TechTeam/data.js
@@ -3,54 +3,54 @@ export const persons = [
   {
     name: "Prayag Sharma",
     role: "Server-Side Development",
-    img: "/techteam/prayag.jpg", 
+    img: "/techteam/prayag.jpg",
     mob: "9620788383",
     github: "https://github.com/prg2308",
-    linkedin: "https://www.linkedin.com/in/prayag-sharma-78aa74213",  
+    linkedin: "https://www.linkedin.com/in/prayag-sharma-78aa74213",
     insta: "https://instagram.com/prg2308"
   },
- 
+
   {
     name: "Swasthik Shetty",
     role: "FullStack Development",
-    img: "/techteam/swasthik.jpeg", 
+    img: "/techteam/swasthik.jpeg",
     mob: "6364172219",
     github: "https://github.com/swasthikshetty10",
-    linkedin: "https://www.linkedin.com/in/swasthikshetty10/",  
+    linkedin: "https://www.linkedin.com/in/swasthikshetty10/",
     insta: "#"
   },
 
   {
     name: "Anjuman Raj",
     role: "FullStack Development",
-    img: "/techteam/anjuman.jpeg", 
+    img: "/techteam/anjuman.jpeg",
     mob: "9649409088",
     github: "https://github.com/ayusmann",
-    linkedin: "https://www.linkedin.com/in/anjuman-raj-ba390b1b5/",  
+    linkedin: "https://www.linkedin.com/in/anjuman-raj-ba390b1b5/",
     insta: "https://www.instagram.com/ayusman23_/"
   },
   {
     name: "Numan Naeem",
     role: "UI Design",
-    img: "/techteam/numan.jpeg", 
+    img: "/techteam/numan.jpeg",
     mob: "8606521714",
     github: "https://github.com/numannaeem",
-    linkedin: "https://www.linkedin.com/in/numanhere",  
+    linkedin: "https://www.linkedin.com/in/numanhere",
     insta: "https://instagram.com/num4n_"
   },
   {
     name: "Dhanish S Suvarna",
     role: "Back-End Development",
-    img: "/techteam/dhanish.jpg", 
+    img: "/techteam/dhanish.jpg",
     mob: "8073445263",
     github: "https://github.com/dhanishssuvarna",
-    linkedin: "https://www.linkedin.com/in/dhanishssuvarna/",  
+    linkedin: "https://www.linkedin.com/in/dhanishssuvarna/",
     insta: "https://instagram.com/dhanish_s_suvarna"
   },
   {
     name: "Thanmay Das",
     role: "Front-End Development",
-    img: "/techteam/tanmay.jpeg", 
+    img: "/techteam/tanmay.jpeg",
     mob: "8431439127",
     github: "https://github.com/tAnboyy",
     linkedin: "https://www.linkedin.com/in/thanmay-m-das-676240193",
@@ -60,29 +60,29 @@ export const persons = [
   {
     name: "Sonika Shetty",
     role: "Front-End Development",
-    img: "/techteam/sonika.jpeg", 
+    img: "/techteam/sonika.jpeg",
     mob: "#",
     github: "https://github.com/Rubix64",
-    linkedin: "https://www.linkedin.com/in/sonika-shetty-522691212",  
+    linkedin: "https://www.linkedin.com/in/sonika-shetty-522691212",
     insta: "#"
   },
   {
     name: "Shifali U",
     role: "Front-End Development",
-    img: "/techteam/hacker.jpg", 
+    img: "/techteam/shifali.jpeg",
     mob: "#",
     github: "https://github.com/ushifali",
-    linkedin: "https://www.linkedin.com/in/thanmay-m-das-676240193",  
+    linkedin: "https://www.linkedin.com/in/thanmay-m-das-676240193",
     insta: "#"
   },
   {
     name: "Bharath M Kulkarni",
     role: "Development Lead",
-    img: "/techteam/bharath.jpg", 
+    img: "/techteam/bharath.jpg",
     mob: "8861885124",
     github: "https://github.com/BharathMKulkarni",
-    linkedin: "https://www.linkedin.com/in/bharath-m-kulkarni-2778b6112/",  
+    linkedin: "https://www.linkedin.com/in/bharath-m-kulkarni-2778b6112/",
     insta: "https://instagram.com/bharathmkulkarni"
   },
-  
+
 ];
