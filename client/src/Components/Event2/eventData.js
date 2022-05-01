d02ca2030c458b544542d310ae149fa9136e735b
Bharath M Kulkarni <bharathmkulkarni@gmail.com>
2022-05-01 08:52:53 +0530

    updated eventData.js

--- client/src/Components/Event2/eventData.js
+++ client/src/Components/Event2/eventData.js
@@ -4731,17 +4731,18 @@ export const eventData = [
         "Time": "9:00 am",
         "venue": "Infront of NC Block, Survery Lab",
         "branch": "MECH",
-        "image": "/events/Mech/engineoverhaulin.jpg",
+        "image": "/events/Mech/Robosoccer.jpg",
         "smallDesc": "Beckham who? Get those motors smoking; remember the robots from Robowars? Now they play soccer.",
         "fullDesc": "Beckham who? Get those motors smoking; remember the robots from Robowars? Now they play soccer.",
-        "roundDetails": ["In the  beginning, a  toss will decide the first hold over the ball.The winner of the toss will get possession of the ball at the center of the arena, whereas the opponent will have to defend the post.",
-            "The match will consist of t wo halves",
+        "roundDetails": {
+            "Round Details" : ["In the  beginning, a toss will decide the first hold over the ball.The winner of the toss will get possession of the ball at the center of the arena, whereas the opponent will have to defend the post.",
+            "The match will consist of two halves",
             "The duration of one half will depend on the type of match as follows:",
             "Initial knockout matches: 2 minutes",
             "Quarter and semi - final: 5 minutes",
             "Final: 5 minutes",
             "In case of a draw, an extra time of two minutes will be given",
-        ],
+        ]},
 
         "botspecifications": ["Bots can be wired or wireless",
             "Wireless bots should have a minimum range of 12 feet",
@@ -4761,9 +4762,19 @@ export const eventData = [
 
 
         "details": [
-            "The soccer bot can only transform to its size at the begin ning.",
+           
+
+
+        ],
+        "rules": [
+            "Maximum of 4 members and minimum of 2 members per team",
+            "Members of the team must belong to the same institution.",
+            "Students from different colleges cannot form a team",
+            "A maximum of 3 teams are allowed from each college (does not apply to home college)",
+            "The students must carry valid student ID cards of their college which they will be required to produce at the time of registration",
+            "The soccer bot can only transform to its size at the beginning.",
             "It cannot divide later into multiple parts",
-            "Once the weight of the bot is finalized no mo difica tions in the weight will be allowed",
+            "Once the weight of the bot is finalized no modifications in the weight will be allowed",
             "The weight of the bot will be checked before starting each match",
             "Each team will be given 2 minutes at the beginning of each half to make any changes or modifications to the bot(modification in weight is not allowed)",
             "In the beginning, the bot must touch or hit the ball within 10 seconds.",
@@ -4773,13 +4784,13 @@ export const eventData = [
             "If the team does not repair the bot within this time, then it will be disqualified",
             "No stalling of any kind is allowed",
             "All the bots must have their own power supply",
-            "Bots will be analyzed by the referee before each match.He will select or reject the bots, taking into consideration the rules and dimension limits",
+            "Bots will be analyzed by the referee before each match. He will select or reject the bots, taking into consideration the rules and dimension limits",
             "The match will be paused in case of any entanglement of wires of both the bots.If the ball is immobile for 10 seconds then the game will be reset and the ball will be declared as the common ball",
             "The ball may not be lifted in the air by the bot",
             "In case only one team is disqualified, the opponent team wins by default",
             "The d ecisionstaken by the referees will be final",
-            "In case of any dispute s, the decisions of the referees or event managers will be final",
-            "For participation, each team should have their own bot of specified dimensions Ready- made kits are not allowed.A team is not permitted to compete with more than onebot during one match",
+            "In case of any disputes, the decisions of the referees or event managers will be final",
+            "For participation, each team should have their own bot of specified dimensions Ready-made kits are not allowed. A team is not permitted to compete with more than onebot during one match",
             "Only 2 members of each team may be present at a time in the arena",
             "If the bot is wired, one member will be the controller of the bot and the other will hold the wire",
             "The structure of the robot should not change during the competition(E.g.If you have a wedge at the front, you can't convert it into a claw or vice versa)",
@@ -4787,19 +4798,10 @@ export const eventData = [
             "One member of the team must be present near the arena or the room provided at any given time",
             "Teams must be ready before the mentioned time.",
             "A warning will be given after every 2 mins of calling the team, on the third call the team will be disqualified",
-            "The bot should not be disassembled until the result s are declared",
+            "The bot should not be disassembled until the results are declared",
             "If a foul is committed, and a goal occurs before the play is restarted for the foul, the goal is not counted",
-            "Pulling the bot with connected wires on the arena will lead to disqualifica tion",
+            "Pulling the bot with connected wires on the arena will lead to disqualification",
             "All the decisions taken by the organizing team will be deemed final, and no further changes will be encouraged.They will hold full authority to change any of the above rules as per the circumstances",
-
-
-        ],
-        "rules": [
-            "Maximum of 4 members and minimum of 2 members per team",
-            "Members of the team must belong to the same institut ion.",
-            "Students from different colleges cannot form a team",
-            "A maximum of 3 t eams are allowed from each college(does not apply to home college)",
-            "The students must carry valid student ID cards of their college which they will be required to produce at the time of registration",
 
 
         ],
@@ -4818,12 +4820,12 @@ export const eventData = [
 
                 "name": "Yajnesh J Sanil",
                 "phoneNumber": "7892415068",
-                "emailID": "ya jneshjsani 113@g mai l.com "
+                "emailID": "yajneshjsani113@gmail.com"
             },
             {
                 "name": "Walsh William Lewis",
                 "phoneNumber": "9108227035",
-                "emailID": "wa lshlewis1234 @g mai l.co m ",
+                "emailID": "walshlewis1234@gmail.com",
             }
 
 
