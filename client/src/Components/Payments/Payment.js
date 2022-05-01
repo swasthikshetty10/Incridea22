44b9c80d768366e9b154f9304c4f16ae28c40504
Numan Naeem <numan.naeem9@gmail.com>
2022-05-01 22:58:23 +0530

    UI

--- client/src/Components/Payments/Payment.js
+++ client/src/Components/Payments/Payment.js
@@ -17,7 +17,7 @@ function Payment(props) {
 			})
 			const { data } = response
 			const options = {
-				"key": "rzp_live_oC59fGEg79wjse", // Change during production or use document.domain
+				"key": "rzp_live_URKcC3N1btOXKs", // Change during production or use document.domain
 				"amount": data.amount.toString(),
 				"currency": data.currency,
 				"name": "Incridea Registration", //Change this stuff
