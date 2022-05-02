5a54a7cc46950dec7f3ae0b4b61e48f572411350
BharathMKulkarni <bharathmkulkarni@gmail.com>
2022-05-02 07:19:47 +0530

    added nicknames

--- client/src/Components/Profile/nickNames.js
+++ client/src/Components/Profile/nickNames.js
@@ -1,7 +1,13 @@
 export const nickNames = {
-	"4nm18cs094@nmamit.in":"More Gay",
-	"4nm18cs036" : "",
-	"4nm18bt066" : "",
-	
-
-}
+  "4nm18cs094@nmamit.in": "Agent No. 69",
+  "4nm18bt066": "Pronite Artist Incridea23",
+  "4nm18cv080": "Daddy",
+  "4nm18ec003": "Abhi Cheta...",
+  "4nm19cs050": "Mama-25",
+  "4nm18is023": "Chechi...",
+  "4nm18cv112": "Wenu Puplesh",
+  "4nm19is099": "Drone Prathap",
+  "4nm19ec008": "ADB",
+  "4nm18cs149": "Now let me tell you something",
+  "4nm18me121": "Nolan Gomes",
+};
