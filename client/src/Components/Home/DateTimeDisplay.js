3458e0c54afaec107e65a181cdd22b26d110dcf6
Numan Naeem <numan.naeem9@gmail.com>
2022-05-10 13:21:58 +0530

    1 Day/s

--- client/src/Components/Home/DateTimeDisplay.js
+++ client/src/Components/Home/DateTimeDisplay.js
@@ -4,7 +4,7 @@ const DateTimeDisplay = ({ value, type, isDanger }) => {
     return (
         <div className={`p-2 font-nav sm:p-3 lg:p-4 bg-opacity-30 backdrop-blur-lg rounded-md bg-gray-800     ${isDanger ? 'text-red-500 ' : 'text-igold'}`}>
             <div className='text-5xl sm:text-6xl  md:text-7xl xl:text-8xl '>{value < 10 ? `0${value}` : value}</div>
-            <span className='text-[14px] sm:text-base text-gray-100/80'>{type}</span>
+            <span className='text-[14px] sm:text-base text-gray-100/80'>{value === 1 ? type.slice(0,-1) : type}</span>
         </div>
     );
 };
