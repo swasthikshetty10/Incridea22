6c9ac1ae7c01c63ddbf03a373ce84d8d3621aab5
Prayag Sharma <85249351+prg2308@users.noreply.github.com>
2022-11-04 20:39:23 +0530

    Tech team route edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route, useNavigate , Navigate } from "react-router-dom";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import Event2 from "./Components/Event2";
 import Gallery from "./Components/Gallery2";
 import Home from "./Components/Home";
@@ -48,12 +48,12 @@ function App() {
             <Route exact path="/Sponsors" element={<Sponsor />} />
             <Route exact path="/team" element={<Team />} />
             <Route exact path="/pronite" element={<Pronite />} />
-            <Route exact path="/TechTeam" element={<TechTeam />} />
+            {/* <Route exact path="/TechTeam" element={<TechTeam />} /> NOTE: Remove link comment*/}
             <Route path="/login" element={<LoginForm />} />
             <Route path="/forgot" element={<ForgotPass />} />
             <Route exact path="/register/:email" element={<Register />} />
             <Route exact path="/profile" element={<Profile />} />
-            <Route exact path="/mi" element={<Mi/>} />
+            <Route exact path="/mi" element={<Mi />} />
             <Route exact path="/privacy" element={<Privacy />} />
             <Route exact path="/rules" element={<Rules />} />
             {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
@@ -67,11 +67,11 @@ function App() {
 
 export default App;
 
-function Mi(){
-  const navigate=useNavigate()
-  useEffect(()=>{
+function Mi() {
+  const navigate = useNavigate()
+  useEffect(() => {
     navigate("/mi.html")
     window.location.reload()
-   })
+  })
   return <></>
 }
--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -8,68 +8,68 @@ function Footer() {
   const location = useLocation();
 
   return (
-    location.pathname === '/login' ? <></> :   
-     <>
-      <footer className={"p-4 font-body gap-2 z-100 bg-black flex flex-col md:flex-row flex-wrap  justify-center items-center md:justify-between md:p-6"}>
-        <div>
-          <img className="h-12" src={logo} alt="logo" />
-        </div>
-        <div className="text-sm text-gray-400 text-center">
-          <p className="mb-1">
+    location.pathname === '/login' ? <></> :
+      <>
+        <footer className={"p-4 font-body gap-2 z-100 bg-black flex flex-col md:flex-row flex-wrap  justify-center items-center md:justify-between md:p-6"}>
+          <div>
+            <img className="h-12" src={logo} alt="logo" />
+          </div>
+          <div className="text-sm text-gray-400 text-center">
+            {/* <p className="mb-1">
             <Link
               to="/techteam"
               className="transition-all ease-out delay-75 text-[#cd9b58] hover:tracking-wider text-base"
             >
               Made with ♥ by Technical Team, Incridea
             </Link>
-          </p>
-          <Link
-            className="text-gray-400 transition-all ease-out delay-75 hover:text-gray-100"
-            to="/privacy"
-          >
-            {" "}
-            Privacy policy{" "}
-          </Link>{" "}
-          |{" "}
-          <Link
-            className="text-gray-400 transition-all ease-out delay-75 hover:text-gray-100"
-            to="/rules"
-          >
-            {" "}
-            Terms & Conditions{" "}
-          </Link>{" "}
-          |{" "}
-          <Link
-            className="text-gray-400 transition-all ease-out delay-75 hover:text-gray-100"
-            to="/"
-          >
-            {" "}
-            ©&nbsp;Incridea&nbsp;2022{" "}
-          </Link>
-        </div>
-        <div className="flex mx-3 gap-4">
-          <a
-            href="https://instagram.com/incridea"
-            className="place-items-center text-white text-xl"
-          >
-            <FaInstagram className="h-8 hover:text-igold" />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCjvGyATGpvX5dsfIUwEOt1g"
-            className="place-items-center text-white  text-xl "
-          >
-            <FaYoutube className="h-8 hover:text-igold" />
-          </a>
-        </div>
-        {/* <ul className="flex flex-wrap  items-center mt-3 justify-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+          </p> NOTE: Remove link comment*/}
+            <Link
+              className="text-gray-400 transition-all ease-out delay-75 hover:text-gray-100"
+              to="/privacy"
+            >
+              {" "}
+              Privacy policy{" "}
+            </Link>{" "}
+            |{" "}
+            <Link
+              className="text-gray-400 transition-all ease-out delay-75 hover:text-gray-100"
+              to="/rules"
+            >
+              {" "}
+              Terms & Conditions{" "}
+            </Link>{" "}
+            |{" "}
+            <Link
+              className="text-gray-400 transition-all ease-out delay-75 hover:text-gray-100"
+              to="/"
+            >
+              {" "}
+              ©&nbsp;Incridea&nbsp;2022{" "}
+            </Link>
+          </div>
+          <div className="flex mx-3 gap-4">
+            <a
+              href="https://instagram.com/incridea"
+              className="place-items-center text-white text-xl"
+            >
+              <FaInstagram className="h-8 hover:text-igold" />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCjvGyATGpvX5dsfIUwEOt1g"
+              className="place-items-center text-white  text-xl "
+            >
+              <FaYoutube className="h-8 hover:text-igold" />
+            </a>
+          </div>
+          {/* <ul className="flex flex-wrap  items-center mt-3 justify-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
          
           <li>
           </li>
           <li>
           </li>
         </ul> */}
-      </footer>
-    </>
+        </footer>
+      </>
   );
 }
 
